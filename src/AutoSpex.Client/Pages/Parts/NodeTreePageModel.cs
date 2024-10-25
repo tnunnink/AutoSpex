db360ed658d728411488af777045456a85463159
tnunnink <timothyjnunnink@gmail.com>
2024-10-25 09:32:31 -0500

    Added the Find message back into observer so that the new suppression dialog and get list of spec node in memory.

--- src/AutoSpex.Client/Pages/Parts/NodeTreePageModel.cs
+++ src/AutoSpex.Client/Pages/Parts/NodeTreePageModel.cs
@@ -19,7 +19,8 @@ public partial class NodeTreePageModel : PageViewModel,
     IRecipient<Observer.Deleted>,
     IRecipient<Observer.Renamed>,
     IRecipient<Observer.GetSelected>,
-    IRecipient<Observer.Get<NodeObserver>>
+    IRecipient<Observer.Get<NodeObserver>>,
+    IRecipient<Observer.Find<NodeObserver>>
 {
     public ObserverCollection<Node, NodeObserver> Nodes { get; } = [];
     public ObservableCollection<NodeObserver> Selected { get; } = [];
@@ -126,11 +127,35 @@ public partial class NodeTreePageModel : PageViewModel,
             {
                 if (msg.Predicate(node))
                 {
-                    msg.Reply(node);
+                    msg.Reply(new NodeObserver(node));
                     return;
                 }
 
                 BroadcastNode(node.Nodes, msg);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles the request to find in memory node instances that passes the provided predicate condition.
+    /// Since the node tree contains all nodes in the app, we only handle this message here and not from node itself,
+    /// because there could be many instances of the "same" node alive in the app.
+    /// </summary>
+    public void Receive(Observer.Find<NodeObserver> message)
+    {
+        BroadcastNodes(Nodes, message);
+        return;
+
+        void BroadcastNodes(IEnumerable<NodeObserver> nodes, Observer.Find<NodeObserver> msg)
+        {
+            foreach (var node in nodes)
+            {
+                if (msg.Predicate(node))
+                {
+                    msg.Reply(new NodeObserver(node));
+                }
+
+                BroadcastNodes(node.Nodes, msg);
             }
         }
     }
