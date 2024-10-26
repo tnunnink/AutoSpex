66a0b6eace111be93fcae5cb3b46d6b8c4b559f0
tnunnink <timothyjnunnink@gmail.com>
2024-10-25 20:34:34 -0500

    Got a start on the HistoryDetailPage.axaml

---
+++ src/AutoSpex.Client/Pages/Details/HistoryDetailPageModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using AutoSpex.Client.Observers;
+using AutoSpex.Client.Shared;
+using AutoSpex.Engine;
+using AutoSpex.Persistence;
+using JetBrains.Annotations;
+
+namespace AutoSpex.Client.Pages;
+
+[UsedImplicitly]
+public class HistoryDetailPageModel() : DetailPageModel("History")
+{
+    public ObserverCollection<Run, RunObserver> Runs { get; } = [];
+
+    public ObservableCollection<RunObserver> Selected { get; } = [];
+
+    public override async Task Load()
+    {
+        var runs = await Mediator.Send(new ListRuns());
+        Runs.Bind(runs.ToList(), r => new RunObserver(r));
+        RegisterDisposable(Runs);
+    }
+
+    protected override void FilterChanged(string? filter)
+    {
+        Runs.Filter(filter);
+    }
+}
