18bb5c084e4859732b794d22f347718fc475d1af
tnunnink <timothyjnunnink@gmail.com>
2024-10-29 10:11:42 -0500

    Added run persistence and the run again command which will reload a new run instance and trigger execution.

--- src/AutoSpex.Client/Pages/Details/RunDetailPageModel.cs
+++ src/AutoSpex.Client/Pages/Details/RunDetailPageModel.cs
@@ -1,25 +1,54 @@
-﻿using System.Threading.Tasks;
-using AutoSpex.Client.Observers;
+﻿using AutoSpex.Client.Observers;
 using AutoSpex.Client.Shared;
+using AutoSpex.Engine;
+using AutoSpex.Persistence;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using JetBrains.Annotations;
 
 namespace AutoSpex.Client.Pages;
 
 [UsedImplicitly]
-public class RunDetailPageModel(RunObserver run, bool runOnLoad = false) : DetailPageModel(run.Name)
+public partial class RunDetailPageModel(RunObserver run) : DetailPageModel(run.Name)
 {
     public override string Route => $"{nameof(Run)}/{Run.Id}";
     public override string Icon => nameof(Run);
-    public RunObserver Run { get; } = run;
-    public Task<OutcomesPageModel> OutcomePage => Navigator.Navigate(() => new OutcomesPageModel(Run));
+    public RunObserver Run { get; private set; } = run;
+
+    [ObservableProperty] private OutcomesPageModel? _outcomePage;
 
     /// <inheritdoc />
+    /// <remarks>
+    /// This page expects the provided run object to be fully loaded. 
+    /// Only execute runs that have not been run (i.e. new run instances).
+    /// Once the run finishes execution, persist the result, and notify the user.
+    /// </remarks>
     public override async Task Load()
     {
-        //Auto trigger run execution when signaled assuming the run is configured.
-        if (runOnLoad && Run.CanExecute())
-        {
-            await Run.Execute();
-        }
+        OutcomePage = await Navigator.Navigate(() => new OutcomesPageModel(Run));
+
+        if (Run.Result != ResultState.None) return;
+        await Run.Execute();
+
+        var result = await Mediator.Send(new PostRun(Run));
+        if (Notifier.ShowIfFailed(result)) return;
+
+        Notifier.ShowSuccess("Run completed successfully",
+            $"{Run.Node.Name} {Run.Result} for {Run.Source.Name} in {Run.Duration} ms.");
+    }
+
+    [RelayCommand]
+    private async Task RunAgain()
+    {
+        //Create a new run instance.
+        var result = await Mediator.Send(new NewRun(Run.Node.Id, Run.Source.Id));
+        if (Notifier.ShowIfFailed(result)) return;
+
+        //Replace the current run object.
+        Run = new RunObserver(result.Value);
+        OnPropertyChanged(nameof(Run));
+
+        //Reload this page, which will execute the new run instance.
+        await Load();
     }
 }
