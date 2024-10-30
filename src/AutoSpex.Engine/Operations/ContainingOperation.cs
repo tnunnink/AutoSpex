ffae6b65be3c241642e25b78b2f94c55edecd7e7
tnunnink <timothyjnunnink@gmail.com>
2024-10-30 14:50:08 -0500

    Fixed ContainingOperation.cs code to work with collections and type that supports ToString()

--- src/AutoSpex.Engine/Operations/ContainingOperation.cs
+++ src/AutoSpex.Engine/Operations/ContainingOperation.cs
@@ -6,12 +6,15 @@ public class ContainingOperation() : BinaryOperation("Containing")
 {
     protected override bool Evaluate(object? input, object value)
     {
-        return input switch
+        if (input is IEnumerable enumerable and not string)
         {
-            string text when value is string segment => text.Contains(segment),
-            IEnumerable enumerable => enumerable.Cast<object>().Contains(value),
-            _ => false
-        };
+            return enumerable.Cast<object>().Contains(value);
+        }
+
+        //We need to call ToString because not all types we consider "text" are literaly string.
+        var text = input?.ToString();
+        var expected = value.ToString()!;
+        return text is not null && text.Contains(expected);
     }
 
     protected override bool Supports(TypeGroup group) => group == TypeGroup.Text || group == TypeGroup.Collection;
