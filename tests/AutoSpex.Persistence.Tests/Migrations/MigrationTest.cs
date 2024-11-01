64b0ae905de9da2d4670863086542a0e1fa2bc38
tnunnink <timothyjnunnink@gmail.com>
2024-11-01 08:29:03 -0500

    Started a migration test structure. Need to add ways for seeding and getting data. We might just want to use mediator but can't use test context as it call migration.

---
+++ tests/AutoSpex.Persistence.Tests/Migrations/MigrationTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoSpex.Persistence.Tests.Migrations;
+
+public abstract class MigrationTest
+{
+    protected MigrationTest()
+    {
+        var provider = BuildServiceProvider();
+        Runner = provider.GetRequiredService<IMigrationRunner>();
+    }
+
+    public IMigrationRunner Runner { get; }
+
+    private static ServiceProvider BuildServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        services.AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                .AddSQLite()
+                .WithGlobalConnectionString("DataSource=app.db")
+                .ScanIn(typeof(ServiceExtensions).Assembly).For.Migrations());
+
+        return services.BuildServiceProvider();
+    }
+}
