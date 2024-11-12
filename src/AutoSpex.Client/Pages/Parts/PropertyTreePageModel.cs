2719985121e42cd08b4bac959710820f254835c6
tnunnink <timothyjnunnink@gmail.com>
2024-11-12 05:41:19 -0600

    Added group to property with icon. Added new PropertyTreePage.axaml as a part that can be opened from flyout.

---
+++ src/AutoSpex.Client/Pages/Parts/PropertyTreePageModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using AutoSpex.Client.Observers;
+using AutoSpex.Client.Shared;
+using AutoSpex.Engine;
+
+namespace AutoSpex.Client.Pages;
+
+public class PropertyTreePageModel : PageViewModel
+{
+    public PropertyTreePageModel(ElementObserver element)
+    {
+        Properties.BindReadOnly(element.GetProperties().ToList());
+        RegisterDisposable(Properties);
+    }
+
+    public ObserverCollection<Property, PropertyObserver> Properties { get; } = [];
+
+    protected override void FilterChanged(string? filter)
+    {
+        Properties.Filter(filter);
+    }
+}
