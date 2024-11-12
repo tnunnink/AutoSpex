81cab999e278973d2256a757b4c01e4ca58ed013
tnunnink <timothyjnunnink@gmail.com>
2024-11-12 05:44:16 -0600

    Added new Range.cs class to handle ternary operations inputs, which will for now just be the between operation. This type allows us to handle the UI value entry in a specific way separate from collection type.

---
+++ src/AutoSpex.Client/Observers/RangeObserver.cs
@@ -0,0 +1,36 @@
+﻿using AutoSpex.Client.Shared;
+using AutoSpex.Engine;
+
+namespace AutoSpex.Client.Observers;
+
+public class RangeObserver : Observer<Range>
+{
+    public RangeObserver(Range model) : base(model)
+    {
+        Min = new ValueObserver(() => Model.Min, ParseValue, v => Model.Min = v);
+        Max = new ValueObserver(() => Model.Max, ParseValue, v => Model.Max = v);
+
+        Track(Min);
+        Track(Max);
+    }
+
+    public ValueObserver Min { get; }
+    public ValueObserver Max { get; }
+
+    /// <summary>
+    /// The function that parses input values into the actual underlying range min/max value we expect.
+    /// all range values should be a date or a number, so we can use those type groups explicitely.
+    /// If the provided value is another value observer, which comes from suggesstions, then use that value.
+    /// </summary>
+    private static object? ParseValue(object? value)
+    {
+        if (value is ValueObserver observer)
+            return observer.Value;
+
+        if (value is not string text) return value;
+
+        if (TypeGroup.Date.TryParse(text, out var date)) return date;
+        if (TypeGroup.Number.TryParse(text, out var number)) return number;
+        return value;
+    }
+}
---
+++ src/AutoSpex.Engine/Range.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AutoSpex.Engine;
+
+/// <summary>
+/// Represents a range of values defined by a minimum and maximum value.
+/// </summary>
+public class Range
+{
+    /// <summary>
+    /// Creates a new default <see cref="Range"/> instance.
+    /// </summary>
+    public Range()
+    {
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="Range"/> initialized with the provided min and max values.
+    /// </summary>
+    /// <param name="min">The minimum value for the range.</param>
+    /// <param name="max">The maximum value for the range.</param>
+    public Range(object? min, object? max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    /// <summary>
+    /// Gets or sets the minimum value for the range.
+    /// </summary>
+    [JsonConverter(typeof(JsonObjectConverter))]
+    public object? Min { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum value for the range.
+    /// </summary>
+    [JsonConverter(typeof(JsonObjectConverter))]
+    public object? Max { get; set; }
+
+    /// <summary>
+    /// Checks if the given value is within the specified range defined by Min and Max values.
+    /// </summary>
+    /// <param name="value">The value to check if it is within the range.</param>
+    /// <returns>True if the value is within the range, false otherwise.</returns>
+    public bool InRange(object? value)
+    {
+        if (value is not IComparable comparable) return false;
+        return comparable.CompareTo(Min) >= 0 && comparable.CompareTo(Max) <= 0;
+    }
+
+    public override string ToString() => $"{Min} and {Max}";
+}
