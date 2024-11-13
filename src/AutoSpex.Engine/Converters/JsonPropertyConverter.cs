9a3bdd08559df9ef7f2674eb86dfd90520caeefe
tnunnink <timothyjnunnink@gmail.com>
2024-11-13 16:14:48 -0600

    Reworked the custom Json serializers to flatten the criterion but also make the complex objects have proper json structure to make reading/updating file easier going forward.

--- src/AutoSpex.Engine/Converters/JsonPropertyConverter.cs
+++ src/AutoSpex.Engine/Converters/JsonPropertyConverter.cs
@@ -5,37 +5,22 @@ namespace AutoSpex.Engine;
 
 public class JsonPropertyConverter : JsonConverter<Property>
 {
-    public override Property? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override Property Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var origin = typeof(object);
-        var path = string.Empty;
+        var key = reader.GetString();
 
-        while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
-        {
-            if (reader.TokenType != JsonTokenType.PropertyName) continue;
-
-            var propertyName = reader.GetString();
-            reader.Read();
-
-            switch (propertyName)
-            {
-                case nameof(Property.Origin):
-                    origin = reader.GetString().ToType();
-                    break;
-                case nameof(Property.Path):
-                    path = reader.GetString();
-                    break;
-            }
-        }
+        var segments = key?.Split(':') ?? [];
+        if (segments.Length != 2)
+            throw new FormatException($"Invalid Property format '{key}'");
+        
+        var origin = segments[0].ToType();
+        var path = segments[1];
 
         return origin is not null ? Property.This(origin).GetProperty(path) : Property.Default;
     }
 
     public override void Write(Utf8JsonWriter writer, Property value, JsonSerializerOptions options)
     {
-        writer.WriteStartObject();
-        writer.WriteString(nameof(Property.Origin), value.Origin.FullName);
-        writer.WriteString(nameof(Property.Path), value.Path);
-        writer.WriteEndObject();
+        writer.WriteStringValue(value.Key);
     }
 }
