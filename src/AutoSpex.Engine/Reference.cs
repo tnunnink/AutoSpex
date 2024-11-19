2c6a596a9457b6605802925e3591fefecec5798a
tnunnink <timothyjnunnink@gmail.com>
2024-11-19 15:22:13 -0600

    Further developed the Reference.cs class

--- src/AutoSpex.Engine/Reference.cs
+++ src/AutoSpex.Engine/Reference.cs
@@ -2,11 +2,62 @@
 
 namespace AutoSpex.Engine;
 
+/// <summary>
+/// A type that can allow us to dynamically specify/retreive a value from a different part of the source file so that
+/// we don't have to statically configure an argument.
+/// </summary>
 public class Reference
 {
-    public Guid SourceId { get; private set; } = Guid.Empty;
+    public const char StartKey = '{';
+    public const char EndKey = '}';
     
-    public Scope Scope { get; private set; } = string.Empty;
+    /// <summary>
+    /// The scope of the logix object to obtain. 
+    /// </summary>
+    public Scope Scope { get; set; } = Scope.Empty;
+    
+    /// <summary>
+    /// The optional property that specifies a sub value of the returned object instance.
+    /// </summary>
+    public Property Property { get; private set; } = Property.Default;
+    
+    /// <summary>
+    /// The element type of the reference. This is parsed from the scope type for which we have a matching element
+    /// that gives us necessary type information.
+    /// </summary>
+    public Element Element => Scope.Type != ScopeType.Null ? Element.FromName(Scope.Type) : Element.Default;
 
-    public LogixElement? Value { get; private set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="property"></param>
+    public void Use(string property)
+    {
+        Property = Element.Property(property);
+    }
+
+    /// <summary>
+    /// Given an object, try to resolve the value based on this configured reference. This assumes the provided object
+    /// is a LogixElement from which we can obtain the root source L5X.
+    /// As of now this is always the case. If it ends up not being the case, we will have to resolve references ahead
+    /// of time so that we can return the correct data.
+    /// </summary>
+    /// <param name="candidate">The candidate object from which to resolve the configured reference.</param>
+    /// <returns>The object that represents the referenced value.</returns>
+    public object? Resolve(object? candidate)
+    {
+        if (candidate is not LogixElement element || element.L5X is null) return null;
+        
+        //We want to use Get because if the scope is invalid we will get and exception and report that to the user.
+        var scoped = element.L5X.Get(Scope);
+        
+        //If configured return the sub property value of the object.
+        return Property == Property.Default ? scoped : Property.GetValue(scoped);
+    }
+    
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return string.Concat(StartKey, Scope, EndKey, '.', Property.Path);
+    }
 }
