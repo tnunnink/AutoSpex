0f2f15034f88bf7dd110c9882eb79f6bcc91eef4
tnunnink <timothyjnunnink@gmail.com>
2024-12-04 13:48:08 -0600

    Reworked the spec migration v2 and tested

--- src/AutoSpex.Engine/Migrations/SpecMigrationV2.cs
+++ src/AutoSpex.Engine/Migrations/SpecMigrationV2.cs
@@ -20,15 +20,12 @@ public class SpecMigrationV2 : ISpecMigration
         using var writer = new Utf8JsonWriter(stream);
 
         writer.WriteStartObject();
+
         writer.WriteNumber("SchemaVersion", Version);
         writer.WriteString("SpecId", root.GetProperty("SpecId").GetString());
 
-        writer.WritePropertyName("Steps");
-        writer.WriteStartArray();
-        WriteQueryElement(writer, root);
-        WriteFilterElement(writer, root);
-        WriteVerifyElement(writer, root);
-        writer.WriteEndArray();
+        WriterQuery(writer, root);
+        WriteVerifications(writer, root);
 
         writer.WriteEndObject();
         writer.Flush();
@@ -37,41 +34,92 @@ public class SpecMigrationV2 : ISpecMigration
     }
 
     /// <summary>
-    /// Migrates the Element property into a Query Step object in the new steps array. 
+    /// Write the new query object with the current element and single filter step.
     /// </summary>
-    private static void WriteQueryElement(Utf8JsonWriter writer, JsonElement root)
+    private static void WriterQuery(Utf8JsonWriter writer, JsonElement root)
     {
+        writer.WritePropertyName("Query");
         writer.WriteStartObject();
-        writer.WriteString("$type", "Query");
         writer.WriteString("Element", root.GetProperty("Element").GetString());
-        writer.WritePropertyName("Criteria");
+        writer.WritePropertyName("Steps");
         writer.WriteStartArray();
+        WriteFilter(writer, root);
         writer.WriteEndArray();
         writer.WriteEndObject();
     }
 
     /// <summary>
-    /// Migrates the Filter criterion array into a Filter Step object in the new steps array. 
+    /// Write the new filter object as a step in the query. 
     /// </summary>
-    private static void WriteFilterElement(Utf8JsonWriter writer, JsonElement root)
+    private static void WriteFilter(Utf8JsonWriter writer, JsonElement root)
     {
         writer.WriteStartObject();
         writer.WriteString("$type", "Filter");
         writer.WriteString("Match", "All");
         writer.WritePropertyName("Criteria");
-        writer.WriteRawValue(root.GetProperty("Filters").GetRawText());
+        WriteCriteria(writer, root.GetProperty("Filters"));
         writer.WriteEndObject();
     }
 
     /// <summary>
-    /// Migrates the Verifications criterion array into a Filter Step object in the new steps array. 
+    /// Write the verification criteria. 
     /// </summary>
-    private static void WriteVerifyElement(Utf8JsonWriter writer, JsonElement root)
+    private static void WriteVerifications(Utf8JsonWriter writer, JsonElement root)
+    {
+        writer.WritePropertyName("Criteria");
+        WriteCriteria(writer, root.GetProperty("Verifications"));
+    }
+
+    /// <summary>
+    /// Migrates a collection of criterion
+    /// </summary>
+    private static void WriteCriteria(Utf8JsonWriter writer, JsonElement element)
+    {
+        writer.WriteStartArray();
+
+        foreach (var criterion in element.EnumerateArray())
+        {
+            MigrateCriterion(writer, criterion);
+        }
+
+        writer.WriteEndArray();
+    }
+
+    /// <summary>
+    /// Writes the criterion object. For each property value strip off the type info.
+    /// </summary>
+    private static void MigrateCriterion(Utf8JsonWriter writer, JsonElement criteria)
     {
         writer.WriteStartObject();
-        writer.WriteString("$type", "Verify");
-        writer.WritePropertyName("Criteria");
-        writer.WriteRawValue(root.GetProperty("Verifications").GetRawText());
+
+        foreach (var property in criteria.EnumerateObject())
+        {
+            if (property.NameEquals("Property"))
+            {
+                var value = property.Value.GetString() ?? string.Empty;
+                var updated = value[(value.IndexOf(':') + 1)..];
+                writer.WriteString("Property", updated);
+                continue;
+            }
+
+            if (property.NameEquals("Argument") && 
+                property.Value.ValueKind != JsonValueKind.Null && 
+                property.Value.GetProperty("Group").GetString() == "Criterion")
+            {
+                var argument = property.Value;
+                writer.WritePropertyName("Argument");
+                writer.WriteStartObject();
+                writer.WriteString("Group", argument.GetProperty("Group").GetString());
+                writer.WritePropertyName("Data");
+                MigrateCriterion(writer, argument.GetProperty("Data"));
+                writer.WriteEndObject();
+                continue;
+            }
+
+            writer.WritePropertyName(property.Name);
+            property.Value.WriteTo(writer);
+        }
+
         writer.WriteEndObject();
     }
 }
